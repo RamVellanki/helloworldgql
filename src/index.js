8347ef2648e317ab593181040bade7c753733eec
Ram Vellanki <vellanki.chandra@gmail.com>
2018-01-14 14:14:54 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import 'source-map-support/register';
 import 'babel-polyfill';
+import express from 'express';
+import bodyParser from 'body-parser';
+import {graphqlExpress} from 'graphql-server-express';
+import {makeExecutableSchema} from 'graphql-tools';
 
-console.log('hello world!');
+import resolvers from './resolvers';
+import schema from './schema';
+
+const app = express();
+
+const executableSchema = makeExecutableSchema({
+	typeDefs: schema,
+	resolvers
+});
+
+app.post('/graphql', 
+	bodyParser.json(),
+	graphqlExpress(() => ({
+			schema: executableSchema
+		})
+	)
+);
+
+app.get('/', function(req, res) {
+	res.send('This GraphQL only supports <a href="/post" > POST </a>');
+});
+app.listen(3456, function() { console.log('App running at 3456 port'); });
---
+++ src/resolvers.js
@@ -0,0 +1,7 @@
+export default {
+	Query: {
+		helloWorld: () => 'Hello World'
+	}
+};
+
+
